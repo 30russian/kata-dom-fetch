0e4b264450b8f7d496bd0bd0eac310a240ffb806
30russian <30russian@gmail.com>
2023-01-13 13:30:19 +0300

    Fixed review notes + fixed missed issues

--- script.js
+++ script.js
@@ -3,6 +3,47 @@ const url = 'https://api.github.com/search/repositories';
 const searchField = document.querySelector('input');
 const projects = document.querySelector('.projects');
 
+class Suggestions {
+  constructor() {
+    this._liRepoMap = new Map();
+    this._suggestions = null;
+    this._clickListener = this.shouldAddProject.bind(this);
+  }
+
+  shouldAddProject(event) {
+    if (this._liRepoMap.has(event.target)) {
+      addProject(this._liRepoMap.get(event.target));
+      searchField.value = '';
+      this.render([]);
+    }
+  }
+
+  render(repos) {
+    if (this._suggestions != null) {
+      this._suggestions.removeEventListener('click', this._clickListener);
+      this._suggestions.remove();
+      this._suggestions = null;
+    }
+
+    if (repos.length === 0) {
+      return;
+    }
+
+    this._suggestions = document.createElement('ul');
+    this._suggestions.classList.add('suggestions');
+    for (let i = 0; i < Math.min(repos.length, 5); i++) {
+      const repo = repos[i];
+      const li = document.createElement('li');
+      this._liRepoMap.set(li, repo);
+      li.textContent = repo.name;
+      this._suggestions.append(li);
+    }
+    this._suggestions.addEventListener('click', this._clickListener);
+
+    searchField.after(this._suggestions);
+  }
+}
+
 function debounce(fn, debounceTime) {
   let timer;
   return function(...args) {
@@ -24,37 +65,6 @@ function addProject(project) {
   projects.append(liClone);
 }
 
-function renderSuggestions(repos) {
-  let suggestions = document.querySelector('.suggestions');
-  if (suggestions != null) {
-    suggestions.remove();
-  }
-
-  if (repos.length === 0) {
-    return;
-  }
-
-  const liRepoMap = new Map();
-  suggestions = document.createElement('ul');
-  suggestions.classList.add('suggestions');
-  for (let i = 0; i < Math.min(repos.length, 5); i++) {
-    const repo = repos[i];
-    const li = document.createElement('li');
-    liRepoMap.set(li, repo);
-    li.textContent = repo.name;
-    suggestions.append(li);
-  }
-  suggestions.addEventListener('click', (event) => {
-    if (liRepoMap.has(event.target)) {
-      addProject(liRepoMap.get(event.target));
-      searchField.value = '';
-      renderSuggestions([]);
-    }
-  })
-
-  searchField.after(suggestions);
-}
-
 async function getRepos(query, onResult) {
   if (!query) {
     onResult([]);
@@ -62,24 +72,22 @@ async function getRepos(query, onResult) {
 
   const encodedUrl = new URL(url);
   encodedUrl.searchParams.set('q', query);
-  // encodedUrl.searchParams.set('sort', 'stars');
+  encodedUrl.searchParams.set('per_page', 5);
   const response = await fetch(encodedUrl, {
     method: 'GET',
     headers: {
-      // 'Content-Type': 'application/json;charset=utf-8'
       'Accept': 'application/vnd.github+json'
     }
-    // body: new URLSearchParams({
-    //   q: query
-    // })
   });
   const result = await response.json();
-  onResult(result.items);
+  onResult(result.items ?? []);
 }
 const debouncedGetRepos = debounce(getRepos, 1000);
 
+const suggestions = new Suggestions();
+suggestions.render([]);
 searchField.addEventListener('input', function(event) {
-  debouncedGetRepos(this.value, renderSuggestions);
+  debouncedGetRepos(this.value, suggestions.render.bind(suggestions));
 });
 
 projects.addEventListener('click', (event) => {
@@ -87,5 +95,3 @@ projects.addEventListener('click', (event) => {
     event.target.closest('li').remove();
   }
 });
-
-renderSuggestions([]);
